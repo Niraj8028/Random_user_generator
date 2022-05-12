0ec1388a13c8c828a64d937b736ee6bea5b6d20c
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-05-12 13:28:59 +0530

    App css added

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React,{useState,useEffect} from "react";
+import { Container,Row,Col } from "reactstrap";
+import  "bootstrap/dist/css/bootstap.min.css";
+import "./App.css"
+import axios from "axios";
+
+const App=()=>{
+
+}
+export default App;
