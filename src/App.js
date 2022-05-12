d310851b03e6c39b1aa88044f563f15308cd52c4
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-05-12 14:05:51 +0530

    Fetch api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React,{useState,useEffect} from "react";
 import { Container,Row,Col } from "reactstrap";
-import  "bootstrap/dist/css/bootstap.min.css";
+
 import "./App.css"
 import axios from "axios";
 
 const App=()=>{
-
+    const[details,setDetails]=useState({});
+    const getDetails= async() =>{
+        const {data}=await axios.get('https://randomuser.me/api/')
+        console.log("response :",data)
+        const details=data.results[0];
+        setDetails(details);
+    }
+    useEffect(()=>{
+        getDetails();
+        
+    },[])
+    return(
+        <h1>Profiles</h1>
+    )
+    
+    
 }
 export default App;
